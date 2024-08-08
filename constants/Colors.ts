38532107ca3318952a8fefe2f64a3979a0046eb5
pawelek <k.pawelek@vp.pl>
2024-08-08 21:23:40 +0200

    add global colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,24 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = '#0c4a6e';
+const tintColorDark = '#f5f5f5';
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: '#082f49',
+    background: '#f5f5f5',
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: '#082f49',
+    tabIconDefault: '#082f49',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: '#f5f5f5',
+    background: '#4c0519',
     tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    icon: '#881337',
+    tabIconDefault: '#881337',
     tabIconSelected: tintColorDark,
   },
 };
